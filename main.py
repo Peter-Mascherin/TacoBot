2b96c2c7c40eea625099be10e4ac00a8d81eb91f
Peter-Mascherin <pe.mascherin@gmail.com>
2021-03-22 13:45:17 -0400

    testing gitignore

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from tkinter.constants import *
 import discord
 import random
+import tkinter as tk
+import time
+import threading
+
 
 client = discord.Client()
 
@@ -7,7 +12,6 @@ client = discord.Client()
 def randomnum():
     num = random.randint(0,100)
     return num
-    
     
 @client.event
 async def on_ready():
@@ -31,4 +35,4 @@ async def on_message(message):
     if message.content.startswith('$help'):
         await message.reply('My current commands are:\n$hello\n$rnum\n$whoami\n$help',mention_author=False)
 
-client.run("oops lol")
+client.run("ODIzMzg0MjA0MTgzNzMyMzA0.YFgCTA.uYTQ2lzZJRWHN0OI5AWXcQA9PoU")
